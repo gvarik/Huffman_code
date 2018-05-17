da178d9a9751f0acdf1b92f620efb10f2db5f680
Vladislav Petrov <p.vlad95@gmail.com>
2018-05-17 20:53:53 +0300

    ver1

---
+++ main.py
@@ -0,0 +1,45 @@
+''''По данной непустой строке s длины не более 104, состоящей из строчных букв латинского алфавита, постройте оптимальный беспрефиксный код'''
+
+import heapq
+from collections import Counter, namedtuple
+
+
+class Node(namedtuple('Node',['left','right'])):
+    def walk(self,code,acc):
+        self.left.walk(code,acc+'0')
+        self.right.walk(code,acc+'1')
+
+
+class Leaf(namedtuple('Leaf',['char'])):
+    def walk(self,code,acc):
+        code[self.char]=acc or '0'
+
+
+def huffman_encode(s):
+    h=[]
+    for ch,freq in Counter(s).items():
+        h.append((freq,len(h),Leaf(ch)))
+    heapq.heapify(h)
+    count=len(h)
+    while len(h)>1:
+        freq1,_count1,left=heapq.heappop(h)
+        freq2, _count2, right = heapq.heappop(h)
+        heapq.heappush(h,(freq1+freq2,count,Node(left,right)))
+        count+=1
+
+    [(_freq,_count,root)]=h
+    code={}
+    root.walk(code,'')
+    return code
+
+def main():
+    s=input()
+    code=huffman_encode(s)
+    encoded=''.join(code[ch] for ch in s)
+    print(len(code),len(encoded))
+    for ch in sorted(code):
+        print('{}: {}'.format(ch,code[ch]))
+    print(encoded)
+
+
+main()
